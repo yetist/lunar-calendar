feb98ba982cae843527e484a90278cc8d75efcb1
Xiaotian Wu <yetist@gmail.com>
2025-05-14 16:40:42 +0800

    Update year of the copyright to 2025

--- modules/module.c
+++ modules/module.c
@@ -1,22 +1,7 @@
-/* vi: set sw=4 ts=4 wrap ai: */
+/* SPDX-License-Identifier: LGPL-2.1-only */
 /*
- * module.c: This file is part of lunar-calendar.
- *
- * Copyright (C) 2018-2022 yetist <yetist@gmail.com>
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library; if not, see <http://www.gnu.org/licenses/>.
- * */
+ * Copyright (C) 2009-2025 yetist <yetist@gmail.com>
+ */
 
 #ifdef HAVE_CONFIG_H
 #include <config.h>
--- tests/test-calendar.c
+++ tests/test-calendar.c
@@ -1,20 +1,6 @@
-/* example-start calendar calendar.c */
+/* SPDX-License-Identifier: LGPL-2.1-only */
 /*
- * Copyright (C) 1998 Cesar Miquel, Shawn T. Amundson, Mattias Grönlund
- * Copyright (C) 2000 Tony Gale
- *
- * This library is free software; you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation; either version 2.1 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this program. If not, see <http://www.gnu.org/licenses/>.
+ * Copyright (C) 2009-2025 yetist <yetist@gmail.com>
  */
 
 #include "config.h"
@@ -49,7 +35,7 @@ enum
 {
   calendar_show_header,
   calendar_show_days,
-  calendar_month_change, 
+  calendar_month_change,
   calendar_show_week,
   calendar_monday_first
 };
